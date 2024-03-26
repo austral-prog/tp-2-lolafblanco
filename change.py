bafd6fbaf6d8b948a5cec1fa9536a0e8dff7d832
lolafblanco <162721872+lolafblanco@users.noreply.github.com>
2024-03-26 15:22:19 -0300

    Update change.py

--- change.py
+++ change.py
@@ -2,5 +2,5 @@ def change():
     expense = 23.75
     money = 100
     print(f"Gasto: {expense} \nDinero recibido: {money}\n")
-    print (f"Vuelto\n\nPesos:\n{int(money - expense)}")
-    print(f"Centavos:\n{(money - expense)-int(money - expense)}")
+    print (f"Vuelto\n\nPesos:\n{int(money-expense)}")
+    print(f"Centavos: \n{(money-expense)-int(money-expense)}")
